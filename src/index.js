577b0e3086380787db05658a113c727ac50f090f
Gabrielle P <114810027+Gaby269@users.noreply.github.com>
2023-09-25 21:18:54 +0200

    Mis à jour de la root

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import ReactDOM from "react-dom/client";
+
 import "../node_modules/axentix/dist/axentix.min.css"; // Importez le fichier CSS de Axentix
 import "./css/index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-]);
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider router={router} />);
+ReactDOM.render(
+  <Router>
+    <Switch>
+      <Route exact path="/" component={App} />
+    </Switch>
+  </Router>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
