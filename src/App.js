ac819b4c35f052d185cb2da3525e24feb36e5f5b
Gabrielle P <114810027+Gaby269@users.noreply.github.com>
2023-09-25 19:48:45 +0200

    Ajout de la carte de présentation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./css/App.css";
 import Header from "./components/layout/Header.js";
+import CartePresentation from "../../PortFolio/src/components/CartePresentation";
 
 function App() {
   return (
     <div className="App">
-      <Header />{" "}
+      <main>
+        <CartePresentation />
+      </main>
     </div>
   );
 }
